51206fea492d252af1516973a55dfba781bebfcb
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-05-28 12:00:44 -0400

    Implemented main algorithm in main module

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { generateMoviesHTML } from './movie-converter.js';
+import { renderMoviesToDOM } from './movie-render.js';
+
+
+const movieHTML = generateMoviesHTML();
+//this line will render it to the DOM
+renderMoviesToDOM(movieHTML);
